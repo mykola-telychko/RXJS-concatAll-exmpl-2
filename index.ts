dc23773dc49593a3f185da118a41210fd066327f
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-06 05:00:27 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import { map, concatAll } from 'rxjs/operators';
 import { interval } from 'rxjs';
+// doc https://www.learnrxjs.io/learn-rxjs/operators/combination/concatall
 
 //create and resolve basic promise
 const samplePromise = (val) => new Promise((resolve) => resolve(val));
-const sPromise = (val) => new Promise((resolve) => resolve(val + 1));
+const addTwoPromise = (val) => new Promise((resolve) => resolve(val + 2));
 
 //emit a value every 2 seconds
 const srcInterval = interval(2000);
 
 const obsObj = srcInterval.pipe(
-  map((val) => sPromise(val)),
+  // map((val) => addTwoPromise(val)),
+  map((val) => samplePromise(val)),
   //merge values from resolved promise
   concatAll()
 );
